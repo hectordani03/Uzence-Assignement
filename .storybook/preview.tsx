969db1c69aa450478e4875bdb7d6a0f659fd0a22
Hector Martinez <hectormartinez@MacBook-Pro-de-Hector.local>
2025-08-20 23:44:59 -0600

    Update storybook configuration

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -16,7 +16,7 @@ const preview: Preview = {
       test: 'todo',
     },
     backgrounds: {
-      default: "light",
+      default: "dark",
       values: [
         { name: "light", value: "#ffffff" },
         { name: "dark", value: "#0a0a0a" },
